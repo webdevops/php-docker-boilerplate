dc0865166da898412a4134b5d6e73913538cc3da
Markus Blaschke <mb@cron.eu>
2015-03-11 00:15:22 +0100

    Fixed post_max_size

--- docker/php-fpm/start.sh
+++ docker/php-fpm/start.sh
@@ -8,7 +8,7 @@ env[TYPO3_CONTEXT] = ${TYPO3_CONTEXT}
 php_value[memory_limit] = 512M
 php_value[max_execution_time] = 300
 php_value[max_input_time] = 300
-php_admin_value[php_post_max_size] = 50M
+php_admin_value[post_max_size] = 50M
 php_admin_value[upload_max_filesize] = 50M
 
 " >> /etc/php5/fpm/pool.d/www.conf
