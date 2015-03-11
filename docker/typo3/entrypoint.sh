a71b180e6f75f03462b39c449567a9abb227070d
Markus Blaschke <mb@cron.eu>
2015-03-11 23:27:12 +0100

    Added automatic mysql client configuration

--- docker/typo3/entrypoint.sh
+++ docker/typo3/entrypoint.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-rm -rf /var/www/typo3temp/*
+echo "[client]
+host=mysql
+user=\"$MYSQL_USER\"
+password=\"$MYSQL_PASSWORD\"
+
+[mysql]
+host=mysql
+user=\"$MYSQL_USER\"
+password=\"$MYSQL_PASSWORD\"
+database=\"$MYSQL_DATABASE\"
+default-character-set=utf8
+local-infile=1
+show-warnings
+auto-rehash
+sigint-ignore
+reconnect
+
+[mysqldump]
+host=mysql
+user=\"$MYSQL_USER\"
+password=\"$MYSQL_PASSWORD\"
+
+" > /root/.my.cnf
+
 /bin/bash -l -c "$*"
