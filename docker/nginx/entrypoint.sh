9ce737b1e2b3aa1f03526f40c5e46dfa024cd252
Markus Blaschke <mb@cron.eu>
2015-03-13 18:34:51 +0100

    Cleanup nginx (changed /var/www to /var/www/html)

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -4,4 +4,4 @@
 /bin/sed -i "s@<FPM_HOST>@${FPM_1_PORT_9000_TCP_ADDR}@" /etc/nginx/sites-enabled/default
 /bin/sed -i "s@<FPM_PORT>@${FPM_1_PORT_9000_TCP_PORT}@" /etc/nginx/sites-enabled/default
 
-/usr/sbin/nginx
+nginx
