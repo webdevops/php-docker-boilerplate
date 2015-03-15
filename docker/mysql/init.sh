4a278e978cb964009446f71dfbd95b8835b25ad0
Markus Blaschke <mb@cron.eu>
2015-03-15 17:46:55 +0100

    Cleanup, Added PerconaDB and MariaDB - choose in dockerfile

--- docker/mysql/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Open root user access for development
-if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-fi
