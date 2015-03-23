770ceb56b8aa7797f39ea06f56f9906f79177748
Markus Blaschke <mb@cron.eu>
2015-03-24 00:12:21 +0100

    Added php.ini to fix NEOS exception

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -14,20 +14,6 @@ echo "
 env[TYPO3_CONTEXT]    = ${TYPO3_CONTEXT}
 env[FLOW_CONTEXT]     = ${FLOW_CONTEXT}
 env[FLOW_REWRITEURLS] = ${FLOW_REWRITEURLS}
-
-php_value[short_open_tag]    = On
-php_value[variables_order]   = 'GPCS'
-php_value[request_order]     = 'GP'
-
-php_value[allow_url_fopen]   = On
-php_value[allow_url_include] = Off
-
-php_value[memory_limit]              = 512M
-php_value[max_execution_time]        = 300
-php_value[max_input_time]            = 300
-php_admin_value[post_max_size]       = 50M
-php_admin_value[upload_max_filesize] = 50M
-
 " >> /etc/php5/fpm/pool.d/www.conf
 
 exec /usr/sbin/php5-fpm --nodaemonize
