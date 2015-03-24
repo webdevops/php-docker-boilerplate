754065673e4da97d7f75f2c9e7472fcac7edf9da
Markus Blaschke <mb@cron.eu>
2015-03-24 21:29:20 +0100

    Cleanup solr init

--- docker/solr/entrypoint.sh
+++ docker/solr/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+###################
+# Move storage to storage container
+###################
 if [ -n "$SOLR_STORAGE" ]; then
     rm -rf    /opt/solr/example/solr/typo3cores/data
     mkdir -p  "$SOLR_STORAGE"
@@ -7,6 +11,16 @@ if [ -n "$SOLR_STORAGE" ]; then
     ln -s     "$SOLR_STORAGE" /opt/solr/example/solr/typo3cores/data
 fi
 
-bash /core-create.sh
+###################
+# Create solr data directories (on demand)
+###################
+grep -o "dataDir=\"[^'\"]*\"" /opt/solr/example/solr/solr.xml | sed -E 's/dataDir="(.+)"/\1/' | while read SOLR_CORE; do
+    mkdir -p  "/opt/solr/example/solr/typo3cores/$SOLR_CORE"
+    chmod 777 "/opt/solr/example/solr/typo3cores/$SOLR_CORE"
+done
 
+
+###################
+# Run solr
+###################
 exec java -jar start.jar
