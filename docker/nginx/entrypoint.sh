287ccbc28a28600289e3796310a7666bd1b0aefc
Markus Blaschke <mblaschke82@gmail.com>
2015-03-31 21:35:58 +0200

    Fixed nginx entrypoint

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -7,20 +7,21 @@
 # Detect correct path of document root
 DOCUMENT_ROOT=$(readlink -f "/docker/$DOCUMENT_ROOT")
 
-cp /etc/nginx/sites-available/default.tpl /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<DOCUMENT_ROOT>@${DOCUMENT_ROOT}@"         /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<TYPO3_CONTEXT>@${TYPO3_CONTEXT}@"         /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<FLOW_CONTEXT>@${FLOW_CONTEXT}@"           /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<FLOW_REWRITEURLS>@${FLOW_REWRITEURLS}@"   /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<FPM_HOST>@${MAIN_PORT_9000_TCP_ADDR}@"    /etc/nginx/sites-enabled/default
-/bin/sed -i "s@<FPM_PORT>@${MAIN_PORT_9000_TCP_PORT}@"    /etc/nginx/sites-enabled/default
+rm -f -- /etc/nginx/conf.d/*.conf
+cp /opt/docker/vhost.conf /etc/nginx/conf.d/vhost.conf
+/bin/sed -i "s@<DOCUMENT_ROOT>@${DOCUMENT_ROOT}@"         /etc/nginx/conf.d/vhost.conf
+/bin/sed -i "s@<TYPO3_CONTEXT>@${TYPO3_CONTEXT}@"         /etc/nginx/conf.d/vhost.conf
+/bin/sed -i "s@<FLOW_CONTEXT>@${FLOW_CONTEXT}@"           /etc/nginx/conf.d/vhost.conf
+/bin/sed -i "s@<FLOW_REWRITEURLS>@${FLOW_REWRITEURLS}@"   /etc/nginx/conf.d/vhost.conf
+/bin/sed -i "s@<FPM_HOST>@${MAIN_PORT_9000_TCP_ADDR}@"    /etc/nginx/conf.d/vhost.conf
+/bin/sed -i "s@<FPM_PORT>@${MAIN_PORT_9000_TCP_PORT}@"    /etc/nginx/conf.d/vhost.conf
 
 #############################
 ## COMMAND
 #############################
 
 if [ "$1" = 'nginx' ]; then
-    exec nginx
+    exec nginx -g "daemon off;"
 fi
 
 exec "$@"
