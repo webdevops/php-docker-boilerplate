6eacc6e6935ee30499c57b69135c37de2081bf31
Markus Blaschke <mblaschke82@gmail.com>
2015-04-29 21:50:04 +0200

    Added alias domains for nginx
    Fixes #39

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -7,6 +7,11 @@
 # Detect correct path of document root
 DOCUMENT_ROOT=$(readlink -f "/docker/$DOCUMENT_ROOT")
 
+ALIAS_DOMAIN=""
+for DOMAIN in $DNS_DOMAIN; do
+    ALIAS_DOMAIN="${ALIAS_DOMAIN} *.${DOMAIN}"
+done
+
 rm -f -- /etc/nginx/conf.d/*.conf
 cp /opt/docker/vhost.conf /etc/nginx/conf.d/vhost.conf
 /bin/sed -i "s@<DOCUMENT_ROOT>@${DOCUMENT_ROOT}@"         /etc/nginx/conf.d/vhost.conf
@@ -16,6 +21,7 @@ cp /opt/docker/vhost.conf /etc/nginx/conf.d/vhost.conf
 /bin/sed -i "s@<FLOW_REWRITEURLS>@${FLOW_REWRITEURLS}@"   /etc/nginx/conf.d/vhost.conf
 /bin/sed -i "s@<FPM_HOST>@${MAIN_PORT_9000_TCP_ADDR}@"    /etc/nginx/conf.d/vhost.conf
 /bin/sed -i "s@<FPM_PORT>@${MAIN_PORT_9000_TCP_PORT}@"    /etc/nginx/conf.d/vhost.conf
+/bin/sed -i "s@<ALIAS_DOMAIN>@${ALIAS_DOMAIN}@"           /etc/nginx/conf.d/vhost.conf
 
 #############################
 ## COMMAND
