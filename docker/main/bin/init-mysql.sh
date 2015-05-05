6571f09acc99c47b083b6a9a0ca1a1b8b7727f13
Markus Blaschke <mblaschke82@gmail.com>
2015-05-05 22:09:12 +0200

    Fixed mysql backup
    Fixes #44

--- docker/main/bin/init-mysql.sh
+++ docker/main/bin/init-mysql.sh
@@ -6,13 +6,13 @@
 
 echo "[client]
 host=mysql
-user=\"$MYSQL_USER\"
-password=\"$MYSQL_PASSWORD\"
+user=\"root\"
+password=\"$MYSQL_ROOT_PASSWORD\"
 
 [mysql]
 host=mysql
-user=\"$MYSQL_USER\"
-password=\"$MYSQL_PASSWORD\"
+user=\"root\"
+password=\"$MYSQL_ROOT_PASSWORD\"
 database=\"$MYSQL_DATABASE\"
 default-character-set=utf8
 local-infile=1
@@ -23,7 +23,7 @@ reconnect
 
 [mysqldump]
 host=mysql
-user=\"$MYSQL_USER\"
-password=\"$MYSQL_PASSWORD\"
+user=\"root\"
+password=\"$MYSQL_ROOT_PASSWORD\"
 
 " | tee /root/.my.cnf > /home/.my.cnf
