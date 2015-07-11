1144e5b157c649fcbfd65a806eca973b161d8602
Markus Blaschke <mblaschke82@gmail.com>
2015-07-11 17:55:37 +0200

    Fixed dnsmasq

--- docker/main/centos-ansible/bin/customization.sh
+++ docker/main/centos-ansible/bin/customization.sh
@@ -5,6 +5,4 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-export DEBIAN_FRONTEND="noninteractive"
-
 ## add your custom stuff here
