45d84a2321f3845f426c054e9bfd9c487efe7382
Markus Blaschke <mblaschke82@gmail.com>
2015-07-12 00:00:24 +0200

    Removed old ubuntu versions

--- docker/main/ubuntu-14.04/bin/init-ssmtp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#############################
-## Init SSMTP
-#############################
-
-sed -i "s/mailhub=.*/mailhub=${MAIL_GATEWAY}/"        /etc/ssmtp/ssmtp.conf
-sed -i "s/#FromLineOverride=.*/FromLineOverride=YES/" /etc/ssmtp/ssmtp.conf
