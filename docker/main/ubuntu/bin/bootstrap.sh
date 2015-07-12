559642df947227d5d357c9d984f73e8d11a8d1b9
Markus Blaschke <mblaschke82@gmail.com>
2015-07-12 16:12:55 +0200

    Added prebuild docker image support

--- docker/main/ubuntu/bin/bootstrap.sh
+++ docker/main/ubuntu/bin/bootstrap.sh
@@ -10,20 +10,23 @@ export DEBIAN_FRONTEND="noninteractive"
 # workaround for slow/freezing apt inside docker(?)
 echo 'Acquire::http::Pipeline-Depth "0";' >> /etc/apt/apt.conf.d/00no-pipeline
 
-# install apt-add-repository if needed
-if [ -z "`which apt-add-repository`" ]; then
+if [ -z "`which ansible`" ]; then
+    # install apt-add-repository if needed
+    if [ -z "`which apt-add-repository`" ]; then
 
-    apt-get update -q
-    apt-get install -y --no-install-recommends lsb-release
+        apt-get update -q
+        apt-get install -y --no-install-recommends lsb-release
 
 
-    if [ "`lsb_release -r -s`" = '12.04' ]; then
-        apt-get install -y python-software-properties
-    else
-        apt-get install -y software-properties-common
+        if [ "`lsb_release -r -s`" = '12.04' ]; then
+            apt-get install -y python-software-properties
+        else
+            apt-get install -y software-properties-common
+        fi
     fi
-fi
 
-apt-add-repository ppa:ansible/ansible
-apt-get update -q
-apt-get install -y ansible python-apt aptitude
+    # Register and install ansible
+    apt-add-repository ppa:ansible/ansible
+    apt-get update -q
+    apt-get install -y ansible python-apt aptitude
+fi
