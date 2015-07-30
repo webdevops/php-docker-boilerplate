af26648be731891cdd0b38613f05a0b18b7a69ad
Markus Blaschke <mblaschke82@gmail.com>
2015-07-30 20:51:34 +0200

    Cleanup common docker main container

--- docker/main/ubuntu/bin/provision.sh
+++ docker/main/ubuntu/bin/provision.sh
@@ -5,7 +5,6 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-export DEBIAN_FRONTEND="noninteractive"
 export PYTHONUNBUFFERED=1
 
 ANSIBLE_DIR='/opt/docker/provision'
