a3a388cb1a5a27574fe3ed63e0fbd65aa1a935bc
Guillaume Camus <guiyomh@users.noreply.github.com>
2017-01-15 14:07:55 +0100

    Fix command line for mysql-backup (#52)
    * :ambulance: :snowflake: Fix command line for mysql-backup
    
    * Fix space in password

--- bin/backup.sh
+++ bin/backup.sh
@@ -26,7 +26,8 @@ case "$1" in
             fi
 
             logMsg "Starting MySQL backup..."
-            dockerExec mysqldump --opt --single-transaction --events --all-databases --routines --comments | bzip2 > "${BACKUP_DIR}/${BACKUP_MYSQL_FILE}"
+            source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../etc/environment.yml"
+            dockerExec mysqldump -h mysql -u root -p"${MYSQL_ROOT_PASSWORD}" --opt --single-transaction --events --all-databases --routines --comments | bzip2 > "${BACKUP_DIR}/${BACKUP_MYSQL_FILE}"
             logMsg "Finished"
         else
             echo " * Skipping mysql backup, no such container"
